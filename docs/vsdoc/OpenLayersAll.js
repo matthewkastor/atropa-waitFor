f9275b5ccd49ef951fcf04280a3996b3b7b28bc3
Matthew Kastor <matthewkastor@gmail.com>
2013-09-29 01:13:00 -0400

    regenerated from source.

--- docs/vsdoc/OpenLayersAll.js
+++ docs/vsdoc/OpenLayersAll.js
@@ -26,6 +26,9 @@
 
     window.atropa = {
         /// <summary></summary>
+        /// <field name="setAsOptionalArg" type="">Required module, the docs for it are in the &lt;code&gt;
+        ///  atropa-setAsOptionalArg/docs&lt;/code&gt; directory where this module 
+        ///  is located.</field>
         /// <field name="waitFor" type="">Polling functions for quick and sloppy work.</field>
         /// <returns type="atropa"/>
                 
