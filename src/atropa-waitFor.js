f1df27788acce48f9a9791d4d56727673a7284c8
Matthew Kastor <matthewkastor@gmail.com>
2014-01-01 13:37:40 -0500

    updated docs

--- src/atropa-waitFor.js
+++ src/atropa-waitFor.js
@@ -6,14 +6,6 @@
  * @namespace Container for all Glorious classes, functions, etc.
  */
 var atropa = require('atropa-header');
-/**
- * Required module, the docs for it are in the <code>
- *  atropa-setAsOptionalArg/docs</code> directory where this module 
- *  is located.
- * @see <a href="../../../node_modules/atropa-setAsOptionalArg/docs/jsdoc/index.html">
- * ../../../node_modules/atropa-setAsOptionalArg/docs/jsdoc/index.html</a>,
- *  unless you installed this dependency manually.
- */
 atropa.setAsOptionalArg = require('atropa-setAsOptionalArg').setAsOptionalArg;
 /// <reference path="../../docs/vsdoc/OpenLayersAll.js"/>
 /*jslint
